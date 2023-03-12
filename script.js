0801a6fe729d90a6d7f855b1966e2f3f5dbdbf27
Bart Barnard <b.barnard@pl.hanze.nl>
2023-03-12 14:02:17 +0100

    Fixed small bug 🐍🐞🪲

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var menuDisplay = document.getElementById('overview').style.display
+
 // MENU
 document.querySelectorAll('#hamburger img').forEach (el => el.addEventListener('click', evt => {
   console.log(evt)
@@ -6,7 +8,7 @@ document.querySelectorAll('#hamburger img').forEach (el => el.addEventListener('
   overview.style.display = overview.style.display=='block' ? 'none' : 'block'
 }))
 
-document.getElementById('main').addEventListener('click', () => document.getElementById('overview').style.display='none' )
+document.getElementById('main').addEventListener('click', () => document.getElementById('overview').style.display=menuDisplay )
 document.querySelectorAll('#hamburger a').forEach( el => el.addEventListener('click', () => {
   document.getElementById('overview').style.display = 'none';
 }))
