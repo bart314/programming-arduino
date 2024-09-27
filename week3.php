c8e58739b89677e8f9da3d11a9be4ac673d3b522
Bart Barnard <b.barnard@pl.hanze.nl>
2024-09-27 09:06:04 +0200

    Hot swap van de opdrachten

--- week3.php
+++ week3.php
@@ -31,62 +31,9 @@
         </ol>
     </section><!-- preperation -->
 
+
   <section id="exercise1">
-    <h2>Exercise 1: servo motor</h2>
-
-    <p>Working with LEDs is fun and all, but sometimes we want some more action than just walking lights. In such a case we use <em>actors</em> instead of <em>sensors</em>. In this exercise, we are going to use a potentiometer to control the angle of rotation of a servo-meter. As always, this is just to introduce you to the general workings in the hope that you can use these techniques in one way or another in your own project.</p>
-
-    <p>Servos are clever devices. Using just one input pin, they receive the position from the Arduino and they go there. Internally, they have a motor driver and a feedback circuit that makes sure that the servo arm reaches the desired position. But what kind of signal do they receive on the input pin?</p>
-
-  <p>It is a square wave. Each cycle in the signal lasts for 20 milliseconds and for most of the time, the value is LOW. At the beginning of each cycle, the signal is HIGH for a time between 1 and 2 milliseconds. At 1 millisecond it represents 0 degrees and at 2 milliseconds it represents 180 degrees. In between, it represents the value from 0–180. This is a very good and reliable method. The graphic below makes it a little easier to understand.</p>
-
-<p class="center">
-  <img src="imgs/pulses-servo.png" alt="Pulses related to the angle of the servo-motor">
-</p>
-
-<p>Luckely, we don't have to create all this machinery by ourselves: Arduino has a build-in library <tt>Servo</tt>, that we can use to our benefit. Have a look at the code below (which you can download <a href="files/servo-exercise.ino">here</a>). Here, we are using this library to rotate the servo to the left and to the right every second.</p>
-
-
-<pre class="code"><code class="language-arduino">// Include the Servo library 
-  #include &lt;Servo.h&gt;
-  // Declare the Servo pin 
-  int servoPin = 3; 
-  // Create a servo object 
-  Servo Servo1; 
-  void setup() { 
-     // We need to attach the servo to the used pin number 
-     Servo1.attach(servoPin); 
-  }
-
-  void loop(){ 
-     // Make servo go to 0 degrees 
-     Servo1.write(0); 
-     delay(1000); 
-     // Make servo go to 90 degrees 
-     Servo1.write(90); 
-     delay(1000); 
-     // Make servo go to 180 degrees 
-     Servo1.write(180); 
-     delay(1000); 
-  }
-  </code></pre>
-
-  <p>Realise the following setup on your breadboard (of course, we don't actually need a breadboard in this case) and upload the code to your Arduino. Make sure you understand what is going on and why the system is doing what it is doing.</p>
-
-  <p class="center">
-    <img src="imgs/servo-breadboard.jpeg" alt="The servo motor connected to the breadboard">
-  </p>
-
-  <p class="center">
-    <img src="imgs/servo-fritzing.png" alt="A model of the setup">
-  </p>
-
-  <p>In the next few exercises we are going to expand on this simple setup.</p>
-
-  </section><!-- exercise 1-->
-
-  <section id="exercise2">
-    <h2>Exercise 2: Serial communication</h2>
+    <h2>Exercise 1: Serial communication</h2>
 
     <p><b>Introduction</b></p>
 
@@ -172,7 +119,60 @@ String getValue(String data, char separator, int index) {
 <p><b>Part 3</b></p>
 
 <p>Now update your code so that instead of just having the chosen LED burning, it is actually blinking. We will expand on this last step in the next exercise.</p>
-  </section><!-- exercise2 -->
+  </section><!-- exercise1 -->
+
+
+  <section id="exercise2">
+    <h2>Exercise 2: servo motor</h2>
+
+    <p>Working with LEDs is fun and all, but sometimes we want some more action than just walking lights. In such a case we use <em>actors</em> instead of <em>sensors</em>. In this exercise, we are going to use a potentiometer to control the angle of rotation of a servo-meter. As always, this is just to introduce you to the general workings in the hope that you can use these techniques in one way or another in your own project.</p>
+
+    <p>Servos are clever devices. Using just one input pin, they receive the position from the Arduino and they go there. Internally, they have a motor driver and a feedback circuit that makes sure that the servo arm reaches the desired position. But what kind of signal do they receive on the input pin?</p>
+
+  <p>It is a square wave. Each cycle in the signal lasts for 20 milliseconds and for most of the time, the value is LOW. At the beginning of each cycle, the signal is HIGH for a time between 1 and 2 milliseconds. At 1 millisecond it represents 0 degrees and at 2 milliseconds it represents 180 degrees. In between, it represents the value from 0–180. This is a very good and reliable method. The graphic below makes it a little easier to understand.</p>
+
+<p class="center">
+  <img src="imgs/pulses-servo.png" alt="Pulses related to the angle of the servo-motor">
+</p>
+
+<p>Luckely, we don't have to create all this machinery by ourselves: Arduino has a build-in library <tt>Servo</tt>, that we can use to our benefit. Have a look at the code below (which you can download <a href="files/servo-exercise.ino">here</a>). Here, we are using this library to rotate the servo to the left and to the right every second.</p>
+
+
+<pre class="code"><code class="language-arduino">// Include the Servo library 
+  #include &lt;Servo.h&gt;
+  // Declare the Servo pin 
+  int servoPin = 3; 
+  // Create a servo object 
+  Servo Servo1; 
+  void setup() { 
+     // We need to attach the servo to the used pin number 
+     Servo1.attach(servoPin); 
+  }
+
+  void loop(){ 
+     // Make servo go to 0 degrees 
+     Servo1.write(0); 
+     delay(1000); 
+     // Make servo go to 90 degrees 
+     Servo1.write(90); 
+     delay(1000); 
+     // Make servo go to 180 degrees 
+     Servo1.write(180); 
+     delay(1000); 
+  }
+  </code></pre>
+
+  <p>Realise the following setup on your breadboard (of course, we don't actually need a breadboard in this case) and upload the code to your Arduino. Make sure you understand what is going on and why the system is doing what it is doing.</p>
+
+  <p class="center">
+    <img src="imgs/servo-breadboard.jpeg" alt="The servo motor connected to the breadboard">
+  </p>
+
+  <p class="center">
+    <img src="imgs/servo-fritzing.png" alt="A model of the setup">
+  </p>
+
+  </section><!-- exercise 2-->
 
 
 
@@ -213,8 +213,8 @@ String getValue(String data, char separator, int index) {
         <h1>This week</h1>
         <ol>
            <li><a href="#preperation">preperation</a></li>
-           <li><a href="#exercise1">servo motor</a></li>
-           <li><a href="#exercise2">serial communication</a></li>
+           <li><a href="#exercise1">serial communication</a></li>
+           <li><a href="#exercise2">servo motor</a></li>
            <li><a href="#exercise3">sensors and actuators</a></li>
            <li><a href="#assignment">assignment</a></li>
         </ol>
